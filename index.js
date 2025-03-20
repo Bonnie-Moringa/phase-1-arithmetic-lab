e1c703bf8ccb614d2e8b2e40876fce437818d7e7
Boniface Mburu <boniface.mburu@student.moringaschool.com>
2025-03-20 13:16:35 +0300

    Arithmrtic operators

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+//multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+//random
+let random = Math.floor(Math.random() * 100) + 1; 
+
+console.log(random);
+
+//mod
+let num3 = 20;
+let num4 = 16;
+let mod = num3 % num4;
+
+console.log(mod); 
+
+//max
+let max = Math.max(15, 20, 5, 10);
+
+console.log(max); 
+
